3b8c46e0b9c3811360bd83f83d959f517f3b510e
nicodeheza <nicolasdeheza@hotmail.com>
2021-12-13 12:48:42 -0300

    banner mobil and queQuines section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 import Header from "../components/header/Header";
 import Carousel from "../components/carousel/Carousel";
+import styles from "../styles/index.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import quienesSomosImg from "../public/img/quienesSomosHome.png";
+import queHacemosImg from "../public/img/queHacemosHome.png";
 
 export default function Home() {
 	return (
@@ -7,6 +12,41 @@ export default function Home() {
 			<Header>
 				<Carousel />
 			</Header>
+			<main>
+				<section className={styles.quienesQueSection}>
+					<div className={styles.quienesQueMainContainer}>
+						<div className={styles.card}>
+							<h1>Quiénes Somos</h1>
+							<p>
+								Somos una agencia que acompaña a profesionales de toda Latinoamérica que
+								quieren estudiar su carrera de posgrado en medicina en Buenos Aires.
+							</p>
+							<Link href="#">
+								<a>¡Conócenos!</a>
+							</Link>
+						</div>
+						<div className={styles.img}>
+							<Image src={quienesSomosImg} alt="Foto Buenos Aires" />
+						</div>
+					</div>
+					<div className={styles.quienesQueMainContainer}>
+						<div className={styles.img}>
+							<Image src={queHacemosImg} alt="Foto Buenos Aires" />
+						</div>
+						<div className={styles.card}>
+							<h1>Qué Hacemos</h1>
+							<p>
+								Te garantizamos el ingreso a las universidades buscando las mejores
+								oportunidades académicas para tu perfil. También te ofrecemos
+								oportunidades laborales para que tu estadía sea sustentable.
+							</p>
+							<Link href="#">
+								<a>Más Información</a>
+							</Link>
+						</div>
+					</div>
+				</section>
+			</main>
 		</>
 	);
 }
